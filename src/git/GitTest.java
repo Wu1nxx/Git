c87fb0aaf6f1de318e9ed103b4237760742c2ca6
Att <1502443909@qq.com>
2022-12-08 18:46:14 +0800

    first commit

---
+++ src/git/GitTest.java
@@ -0,0 +1,15 @@
+package git;
+
+/**
+ * @author ATou
+ * @description
+ * @create 2022/12/8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git1!");
+
+    }
+
+}
